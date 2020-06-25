4fe4631aa4fc02f33419c396879b906c0425591e
nguyenit67 <dehungmanh@gmail.com>
2020-06-25 16:16:20 +0700

    1 file modified

--- zzz_for_fking_stupid_preview_purpose_on_mobile_only_zzz.jsx
+++ zzz_for_fking_stupid_preview_purpose_on_mobile_only_zzz.jsx
@@ -1,43 +1,45 @@
-(function(){
-    const $ = (selector, startNode=document) => 
-                startNode.querySelector(selector);
+<iframe src=>
 
-    const $$ = (selector, startNode=document) => 
-                startNode.querySelectorAll(selector);
+// (function(){
+//     const $ = (selector, startNode=document) => 
+//                 startNode.querySelector(selector);
 
-    function pageModing(url, callbackFun ) {
-      const proxyurl = "https://cors-anywhere.herokuapp.com/";
-      fetch(proxyurl + url)
-      .then(response =>  response.text())
-      .then(callbackFun)
-      .catch(failure => console.log(failure));
-    }
+//     const $$ = (selector, startNode=document) => 
+//                 startNode.querySelectorAll(selector);
 
-    function hideThem(context, bool= true) {
-      let navs = $$("nav", context);
-      ...Array.from($$("nav", context)).slice(-1)
-      let toHide = [
-        $("#UrlBar", context),
-        $(".Console", context),
-      ];
-      console.log(toHide);
-      for(let elem of toHide) {
-        elem.hidden = bool;
-      }
-    }
+//     function pageModing(url, callbackFun ) {
+//       const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//       fetch(proxyurl + url)
+//       .then(response =>  response.text())
+//       .then(callbackFun)
+//       .catch(failure => console.log(failure));
+//     }
 
-    function expoGame(rawHTML) {
-      const parser = new DOMParser();
-      const newdoc = parser.parseFromString(rawHTML, "text/html");
+//     function hideThem(context, bool= true) {
+//       let navs = $$("nav", context); // return NodeList
+//       let toHide = [
+//         $("#UrlBar", context),
+//         $(".Console", context),
+//         navs[ navs.length - 1 ]
+//       ];
+//       console.log(toHide);
+//       for(let elem of toHide) {
+//         elem.hidden = bool;
+//       }
+//     }
 
-      hideThem(newdoc);
+//     function expoGame(rawHTML) {
+//       const parser = new DOMParser();
+//       const newdoc = parser.parseFromString(rawHTML, "text/html");
 
-      document.documentElement
-            .replaceWith(newdoc.documentElement);
-    }
+//       //hideThem(newdoc);
 
-    const url = "https://stackblitz.com/edit/elojs-chapter16-a-platformer-game-for-fun-ahihihihi-current-ver?embed=1&file=zzz_for_fking_stupid_preview_purpose_on_mobile_only_zzz.jsx&hideExplorer=1&hideNavigation=1&view=preview"; 
+//       document.documentElement
+//             .replaceWith(newdoc.documentElement);
+//     }
 
-    pageModing(url, expoGame);
+//     const url = "https://www.w3schools.com/tags/tag_img.asp"; 
 
-}());
+//     pageModing(url, expoGame);
+
+// }());
