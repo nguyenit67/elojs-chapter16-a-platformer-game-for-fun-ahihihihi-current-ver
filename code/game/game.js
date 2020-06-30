c234cc3a4036644f178614b089e74d794c000829
nguyenit67 <dehungmanh@gmail.com>
2020-06-30 23:16:13 +0700

    1 file modified

--- code/game/game.js
+++ code/game/game.js
@@ -54,16 +54,18 @@ function runLevel(level, Display) {
 }
 
 const oldRunLevel = runLevel;
-exports.runLevel = (level, Display) => {
+runLevel = (level, Display) => {
   let display = new Display(document.body, level);
   let state = State.start(level);
   let ending = 1;
+  let running = true;
   return new Promise(resolve => {
     runAnimation(time => {
-      state = state.update(time, arrowKeys);
-      display.syncState(state);
-      resolve(state.status);
-      return false;
+      running ^= arrowKeys.Escape;
+      if (running) {
+        state = state.update(time, arrowKeys);
+        display.syncState(state);
+      }
       if (state.status == "playing") {
         return true;
       } else if (ending > 0) {
