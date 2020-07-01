3bbc3e196dafb5144d998214692f8ee3489621ff
nguyenit67 <dehungmanh@gmail.com>
2020-07-01 11:56:51 +0700

    3 files modified

--- code/actors/CopyCat.js
+++ code/actors/CopyCat.js
@@ -1,12 +1,50 @@
 import {Monster} from "./Monster";
-import {Player} from "./Player";
+import {CopyCat} from "./CopyCat";
 import {Vec} from "~/code/game/game";
 
-export class CopyCat extends Monster {
+const jumpSpeed = 17;
+const gravity = Monster.gravity;
 
+export class CopyCat extends Monster {
+  size = new Vec(0.8, 1.5);
+
+  constructor(pos, speed) {
+    this.pos = pos;
+    this.speed = speed;
+  }  
   get type() { return "copycat"; }
 
-  update() {
-    return Player.prototype.collide.apply(this, arguments);
+  static create(pos) {
+    return new CopyCat(pos.plus(new Vec(0, -0.5)),
+                      new Vec(0, 0));
+  }
+  
+  update(timeStep, state, keys) {
+    let nowPlayer = state.player;
+    let nextPlayer = nowPlayer.update(...arguments);
+    
+    let xSpeed = 0;
+    if (nextPlayer.pos.x !== nowPlayer.pos.x) {
+      xSpeed = nextPlayer.speed.x;    
+    }
+
+    let pos = this.pos;
+    let movedX = pos.plus(
+      new Vec(xSpeed * timeStep, 0));
+
+    if (!state.level.touches(movedX, this.size, "wall")) {
+      pos = movedX;
+    }
+
+    let ySpeed = this.speed.y + timeStep * gravity;
+    let movedY = pos.plus(new Vec(0, ySpeed * timeStep));
+    if (!state.level.touches(movedY, this.size, "wall")) {
+      pos = movedY;
+    } else if (keys.ArrowUp && ySpeed > 0) {
+      ySpeed = -jumpSpeed;
+    } else {
+      ySpeed = 0;
+    }
+    return new CopyCat(pos, new Vec(xSpeed, ySpeed));
   }
 }
